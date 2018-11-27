11d26bd0f89fca1dc8d9f9474787c674f2ed924b
jkerns21 <43002620+jkerns21@users.noreply.github.com>
2018-11-27 08:36:36 -0500

    Add files via upload

--- src/MontyHall.java
+++ src/MontyHall.java
@@ -19,19 +19,6 @@ public class MontyHall
 		int door = keyboard.nextInt();
 		int car = rand.nextInt(3) + 1;
 		
-		if (car == 1)
-		{
-			System.out.println("The car was behind Door #1!");
-		}
-		if (car == 2)
-		{
-			System.out.println("The car was behind Door #2!");
-		}
-		if (car == 3)
-		{
-			System.out.println("The car was behind Door #2!");
-		}
-		
 		if (door == 1)
 		{
 			System.out.println("You picked Door #1.");
@@ -44,9 +31,63 @@ public class MontyHall
 		{
 			System.out.println("You picked Door #3.");
 		}
-		else 
+		
+		
+		
+		if ((car == 1)&&(door == 1))
 		{
-			System.out.println("You picked an invalid door.");
+			System.out.println("There is a goat behind Door #2");
+		}
+		else if ((car == 2)&&(door == 2))
+		{
+			System.out.println("There is a goat behind Door #3");
+		}
+		else if ((car == 3)&&(door == 3))
+		{
+			System.out.println("There is a goat behind Door #1");
+		}
+		else if ((car == 1)&&(door == 2))
+		{
+			System.out.println("There is a goat behind Door #3");
+		}
+		else if ((car == 1)&&(door == 3))
+		{
+			System.out.println("There is a goat behind Door #2");
+		}
+		else if ((car == 2)&&(door == 1))
+		{
+			System.out.println("There is a goat behind Door #3");
+		}
+		else if ((car == 2)&&(door == 3))
+		{
+			System.out.println("There is a goat between Door #1");
+		}
+		else if ((car == 3)&&(door == 1))
+		{
+			System.out.println("There is a goat between Door #2");
+		}
+		else if ((car == 3)&&(door == 2))
+		{
+			System.out.println("There is a goat between Door #1");
+		}
+		
+		System.out.println("Would you like to change your pick? ");
+		String answer = keyboard.nextLine();
+		if (answer.equals("yes"))
+		
+		
+		
+		if (car == 1)
+		{
+			System.out.println("The car was behind Door #1!");
+		}
+		if (car == 2)
+		{
+			System.out.println("The car was behind Door #2!");
+		}
+		if (car == 3)
+		{
+			System.out.println("The car was behind Door #2!");
 		}
 		
 	}
